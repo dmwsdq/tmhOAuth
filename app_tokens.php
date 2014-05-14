4a15e24ef6460c95db2165f57ae3014d608aa715
Darmawan Sidiq <dmwsdq@users.noreply.github.com>
2014-05-14 12:46:27 +0700

    Create app_tokens.php
    Taken directly from Adam Green ebook at 140dev.com

---
+++ app_tokens.php
@@ -0,0 +1,8 @@
+<?php
+// OAuth tokens for @140dev app: 140dev OAuth ebook app
+// You *MUST* replace them with tokens for your app
+$consumer_key = 'yourownkey';
+$consumer_secret = 'yourownkey';
+$user_token = 'yourownkey';
+$user_secret = 'yourownkey';
+?>
